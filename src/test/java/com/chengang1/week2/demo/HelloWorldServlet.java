3983d1ab12438943a1814f2e5dbe8dce41540b08
a'su's <3431318253@qq.com>
2021-03-14 23:40:28 +0800

    HomeWork week2 commit

---
+++ src/test/java/com/chengang1/week2/demo/HelloWorldServlet.java
@@ -0,0 +1,15 @@
+package com.chengang.week2.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class HelloWorldServlet extends HttpSerlet {
+    public void doGet(HttpServletRequest request,HttpServletResponse response)
+            throws IOException {
+        Prinwriter writer =response.getWriter();
+        writer.println("Hello Client!!!");
+    }
+    public void doPost(HttpServletRequest request,HttpServletResponse response){
+
+    }
+}
